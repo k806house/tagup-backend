c3b4c7e252e14701822196e501e92368b78393d9
David Grinberg <DVGrinberg@sberbank.ru>
2021-11-14 02:04:20 +0300

    add TagGenerator

---
+++ tags.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import spacy
+from gensim.models import Word2Vec
+from gensim.models.phrases import Phrases
+from gensim.utils import tokenize
+from spellchecker import SpellChecker
+from transliterate import translit
+
+MODEL_PATH = 'models/word2vec_clean5.model'
+
+
+class TagGenerator:
+    def __init__(self, model_path):
+        self.model = Word2Vec.load(model_path)
+        self.spell_checker = SpellChecker(language='ru')
+        self.nlp = spacy.load("ru_core_news_md")
+
+    def generate(self, query):
+        similar = self.__try_find_similar(query)
+        extended = self.__try_predict_next(query)
+        return similar, extended
+
+    def __try_find_similar(self, query):
+        # print([token.similarity(self.nlp('айфон')) for token in self.nlp('iphone стиралка обувь брюки самсунг samsung')])
+        doc = self.nlp(query)
+        nouns = []
+        for token in doc:
+            if token.pos_ == 'NOUN':
+                nouns.append(token.text)
+        print(nouns)
+
+        proposals = []
+        for noun in nouns:
+            for match, similarity in self.model.wv.most_similar(noun):
+                # print(match, similarity)
+                # if words are identical, e.g. iphone and айфон
+                # if similarity > 0.80:
+                #     continue
+                proposals.append((match, similarity))
+
+        return proposals
+
+    def __try_predict_next(self, query):
+        tokenized = list(tokenize(str(query).lower()))
+        return self.model.predict_output_word(tokenized)
+
+
+if __name__ == '__main__':
+    tg = TagGenerator(MODEL_PATH)
+
+    print(tg.generate('ботинки красные осенние'))
