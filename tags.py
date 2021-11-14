8a76f3af31cf1e3c2b4975e18e69963d94e179e5
David Grinberg <DVGrinberg@sberbank.ru>
2021-11-14 05:26:54 +0300

    better filtering

--- tags.py
+++ tags.py
@@ -1,4 +1,6 @@
+import sys
 from difflib import SequenceMatcher
+from functools import lru_cache
 
 import numpy as np
 import pandas as pd
@@ -10,7 +12,20 @@ from spellchecker import SpellChecker
 from transliterate import translit
 
 MODEL_PATH = 'models/word2vec_clean5.model'
-SIMILARITY_THRESHOLD = 0.8
+SIMILARITY_THRESHOLD = 0.75
+SEMANTIC_SIMILARITY_THRESHOLD = 0.7
+
+
+def contains_fuzzy(s, q):
+    for val in s:
+        if are_similar(val, q):
+            return True
+    return False
+
+
+def are_similar(a, b):
+    similarity = SequenceMatcher(a=a, b=b).ratio()
+    return similarity >= SIMILARITY_THRESHOLD
 
 
 class TagGenerator:
@@ -18,15 +33,22 @@ class TagGenerator:
         self.model = Word2Vec.load(model_path)
         self.spell_checker = SpellChecker(language='ru')
         self.nlp = spacy.load("ru_core_news_md")
+        print("Initialized")
 
     def generate(self, query):
-        print("Initialized")
-        similar = self.__try_find_similar(query)
-        extended = self.__try_predict_next(query)
+        try:
+            similar = self.__try_find_similar(query)
+        except:
+            similar = []
+
+        try:
+            extended = self.__try_predict_next(query)
+        except:
+            extended = []
+
         return similar, extended
 
     def __try_find_similar(self, query):
-        # print([token.similarity(self.nlp('айфон')) for token in self.nlp('iphone стиралка обувь брюки самсунг samsung')])
         doc = self.nlp(query)
         nouns = []
         for token in doc:
@@ -38,14 +60,10 @@ class TagGenerator:
         for noun in nouns:
             used_matches = set()
             for match, semantic_similarity in self.model.wv.most_similar(noun):
-                if match in used_matches:
-                    continue
-                match = self.spell_checker.correction(match)
-                if match in used_matches:
-                    continue
-
-                similarity = SequenceMatcher(a=noun, b=match).ratio()
-                if similarity >= SIMILARITY_THRESHOLD:
+                match = self.__spell_check(match)
+                if semantic_similarity < SEMANTIC_SIMILARITY_THRESHOLD \
+                or are_similar(noun, match) \
+                or contains_fuzzy(used_matches, match):
                     continue
 
                 used_matches.add(match)
@@ -54,22 +72,26 @@ class TagGenerator:
         return proposals
 
     def __try_predict_next(self, query):
-        tokenized = list(tokenize(self.spell_checker.correction(str(query).lower())))
+        tokenized = list(tokenize(self.__spell_check(str(query).lower())))
         used = set()
         proposals = []
         for nxt, confidence in self.model.predict_output_word(tokenized):
-            if nxt in used:
-                continue
-            nxt = self.spell_checker.correction(nxt)
-            if nxt in used:
+            nxt = self.__spell_check(nxt)
+            if contains_fuzzy(tokenized, nxt) or contains_fuzzy(used, nxt):
                 continue
 
             used.add(nxt)
             proposals.append((f'{query} {nxt}', confidence))
         return proposals
 
+    def __spell_check(self, text):
+        return text
+        # its buggy and slow so goodbye
+        return self.spell_checker.correction(text)
+
 
 if __name__ == '__main__':
     tg = TagGenerator(MODEL_PATH)
 
-    print(tg.generate('ботинки красные осенние'))
+    for line in sys.stdin:
+        print(tg.generate(line.strip()))
